2a021458111158db81f048d3d8b84627d91bde80
albuquerque.r.lucas <albuquerque.r.lucas@gmail.com>
2017-06-01 10:07:09 -0300

    Adicionado o algoritmo do exemplo 1

--- js/implementacao1.js
+++ js/implementacao1.js
@@ -1,3 +1,18 @@
 /**
  * Created by Lucas on 01/06/2017.
  */
+
+
+/*$('#element').popover('show');*/
+
+function calcular() {
+
+    var pBA = document.getElementById('pBA').value;
+    var pA = document.getElementById('pA').value;
+    var pB = document.getElementById('pB').value;
+
+    var pAB = (pBA * pA) / pB;
+
+    document.getElementById('resultado').innerHTML = "<b>P(A | B) = " + pAB + "</b>";
+    document.getElementById('conclusao').innerHTML = "<b>Aqui vai ser a conclusão  de " + (pAB * 100) + "\%</b>";
+}
